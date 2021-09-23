f41250bc1f6798305fb2789ccc755732839f0088
Haruhime <iTrqps@protonmail.com>
2021-09-23 09:01:21 -0500

    Update index.js

--- library/index.js
+++ library/index.js
@@ -2,8 +2,16 @@ const path = require('path')
 
 const cClient = require(path.join(__dirname, './classes/client.js'))
 
+const uAccuracy = require(path.join(__dirname, './utils/accuracy.js'))
+
 module.exports = {
 
-  
+    Client: function client(id, secret) {
+        return new cClient(id, secret)
+    },
+
+    accuracy: function accuracy(mode, hit300, hit100, hit50, hit0, hitkatu, hitgeki) {
+        return uAccuracy.accuracy(mode, hit300, hit100, hit50, hit0, hitkatu, hitgeki)
+    }
 
 }
